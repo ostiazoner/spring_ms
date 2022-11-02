fd329264ae3182272cee7905ee02be219eae0859
Victor Agelet <viktorlleida@gmail.com>
2022-11-02 22:28:40 +0100

    implementacion controller profesores 1.1

--- msprofesores/src/main/java/edu/indra/profesores/controller/ProfesoresController.java
+++ msprofesores/src/main/java/edu/indra/profesores/controller/ProfesoresController.java
@@ -2,6 +2,7 @@ package edu.indra.profesores.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import edu.indra.profesores.service.ProfesorService;
 @RequestMapping(name = "/profesores")
 public class ProfesoresController {
 	
+	@Autowired
 	ProfesorService profesorService;
 	
 	@GetMapping("/listarProfesores")
